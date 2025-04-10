f2a409685d232b88461172e72ca33eaf09624cd2
shbradki <sbradkin@gmail.com>
2025-04-10 18:06:23 -0400

    updated About Me section

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,10 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: "I'm Spencer Bradkin. I got my Bachelor's degree in Computer Science from Syracuse University in May 2024.  Since then, I've been working on my NFL Prediction Model, which I hope to have live by the start of the 2025 NFL Season.  I am currently a Headstarter Software Engineer Resident and member of the Breakaway Accelerator program by Rolling Insights.", 
+		bio: "Hi, I'm Spencer Bradkin, a Computer Science graduate from Syracuse University (Class of 2024). Since graduating, I've been focused on building an NFL Prediction Model, which I plan to launch ahead of the 2025 NFL season.",
 	},
 	{
 		id: uuidv4(),
-		bio: 'I love working with ML, AI, and data.  I continuously find ways to learn new things and work with new technologies.  ',
+		bio: "Currently, Im a Software Engineer Resident at Headstarter and a member of the Breakaway Accelerator by Rolling Insights.",
 	},
+	{
+		id: uuidv4(),
+		bio: "I'm passionate about machine learning, AI, and data-driven solutions. I'm always exploring new technologies and pushing myself to learn and grow as a developer.", 
+	},
+
 ];
