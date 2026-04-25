ca5fb991b21d37377c87c34f8f44596b773d779c
shbradki <sbradkin@gmail.com>
2026-04-25 12:52:36 -0400

    Updated home page, also updated about me

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -36,7 +36,7 @@ function AppBanner() {
 					}}
 					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
-					A Software Engineer & ML Enthusiast
+					A Software Engineer & AI Enthusiast
 				</motion.p>
 				<motion.div
 					initial={{ opacity: 0 }}
--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,11 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: "Hi, I'm Spencer Bradkin, a Computer Science graduate from Syracuse University (Class of 2024). Since graduating, I've been focused on building an NFL Prediction Model, which I plan to launch ahead of the 2025 NFL season.",
+		bio: "Hi, I'm Spencer Bradkin, a Computer Science graduate from Syracuse University (Class of 2024). Since graduating, I've been developing Stepscape, an AI-powered walking route generator, built an NFL prediction model, and joined Fantasy+ as Head of Technology.",
 	},
 	{
 		id: uuidv4(),
-		bio: "Currently, Im a Software Engineer Resident at Headstarter and a member of the Breakaway Accelerator by Rolling Insights.",
+		bio: "I was previously a Software Engineering Resident at Headstarter and a member of the Breakway Accelerator at Rolling Insights. I currently teach coding and robotics through RoboFun and computer skills to students with special needs through The Ninth Cloud.",
 	},
 	{
 		id: uuidv4(),
